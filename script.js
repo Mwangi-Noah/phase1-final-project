ef15087aee947f43e48799cfeec635fb497e6cc1
Noah <noahmwangi@gmail.com>
2023-04-12 19:33:00 +0300

    created a modal but it is not working, try something else

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     getAllCountries();
   
 })
+
 //DOM render functions
 
 function renderOneCountry(country) {
@@ -40,4 +41,134 @@ function getAllCountries(country){
     .then(res => res.json())
     .then(countryData => countryData.forEach(country => renderOneCountry(country)));
 }
+//Creating the search functionalities
+const searchBtn = document.getElementById('search');
+const countryInput = document.getElementById('country-input');
 
+searchBtn.addEventListener("click", () => {
+    let countryName = countryInput.value;
+    let countryURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
+    console.log(countryURL);
+    fetch(countryURL)
+        .then((response) => response.json())
+        .then((data) => showModal(data));
+});
+
+function showModal(data) {
+    var modal = document.getElementById("myModal");
+        //Next, we get the span element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+    modalData = document.createElement('div');
+    modalData.innerHTML = `
+        <img src="${data[0].flags.svg}" class="flag-img">
+        <h2>${data[0].name.common}</h2>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Capital:</h4>
+                <span>${data[0].capital[0]}</span>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Continent:</h4>
+                <span>${data[0].continents[0]}</span>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="data-wrapper">
+            <h4>Population:</h4>
+            <span>${data[0].population}</span>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Currency:</h4>
+                <span>${
+                      data[0].currencies[Object.keys(data[0].currencies)].name
+                    } - ${Object.keys(data[0].currencies)[0]}</span>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                 <h4>Common Languages:</h4>
+                <span>${Object.values(data[0].languages)
+                      .toString()
+                      .split(",")
+                      .join(", ")}</span>
+            </div>
+        </div>
+            `;
+        document.querySelector('#modal-body').appendChild(modalData)
+        
+        span.onclick = function() {
+            modal.style.display = "none";
+          }
+          // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+     }}
+    
+        
+    
+
+
+      /*  
+        var modal = document.getElementById("myModal");
+        //Next, we get the span element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+        modalData = document.createElement('div');
+        modalData.innerHTML = `
+            <img src="${data[0].flags.svg}" class="flag-img">
+            <h2>${data[0].name.common}</h2>
+            <div class="wrapper">
+                <div class="data-wrapper">
+                    <h4>Capital:</h4>
+                    <span>${data[0].capital[0]}</span>
+                </div>
+            </div>
+            <div class="wrapper">
+                <div class="data-wrapper">
+                    <h4>Continent:</h4>
+                    <span>${data[0].continents[0]}</span>
+                </div>
+            </div>
+             <div class="wrapper">
+                <div class="data-wrapper">
+                    <h4>Population:</h4>
+                    <span>${data[0].population}</span>
+                </div>
+            </div>
+            <div class="wrapper">
+                <div class="data-wrapper">
+                    <h4>Currency:</h4>
+                    <span>${
+                      data[0].currencies[Object.keys(data[0].currencies)].name
+                    } - ${Object.keys(data[0].currencies)[0]}</span>
+                </div>
+            </div>
+             <div class="wrapper">
+                <div class="data-wrapper">
+                    <h4>Common Languages:</h4>
+                    <span>${Object.values(data[0].languages)
+                      .toString()
+                      .split(",")
+                      .join(", ")}</span>
+                </div>
+            </div>
+            `;
+        document.querySelector('#modal-body').appendChild(modalData)
+        
+        span.onclick = function() {
+            modal.style.display = "none";
+          }
+          // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+     }})
+    
+  */
+    
